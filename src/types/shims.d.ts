58f250efd4e0ed84b50bfcf3ff5d3e4a17e03345
阿沐 <babamu@126.com>
2022-12-03 13:57:10 +0800

    类型声明

---
+++ src/types/shims.d.ts
@@ -0,0 +1,12 @@
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.gif'
+declare module '*.scss'
+declare module '*.ts'
+
+declare module '*.vue' {
+	import type { DefineComponent } from 'vue'
+	const component: DefineComponent<{}, {}, any>
+	export default component
+}
